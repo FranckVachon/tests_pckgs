01cc86d0034d84e4736671c714b1ab564769d517
FranckVachon <francis.vachon@usherbrooke.ca>
2019-03-30 14:01:39 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from tet.test import TestCls
+from projet_pkg.proj_pkg_classes import *
+from projet_pkg.inner_package.inner_classes import InnerClass
+class MainClass:
+    pass
+
+
+if __name__ == '__main__':
+    maincls = MainClass()
+    t = TestCls()
+    project_package_class = ProjPkgClass()
+    inner = InnerClass()
+
+
---
+++ projet_pkg/proj_pkg_classes.py
@@ -0,0 +1,4 @@
+
+
+class ProjPkgClass:
+    pass
